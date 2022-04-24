7f4c3e1898fc39ec6729825cd54cc351533e176b
Fawwazjebeh <94578909+Fawwazjebeh@users.noreply.github.com>
2022-04-24 08:21:41 +0800

    Update settings.js

--- settings.js
+++ settings.js
@@ -18,18 +18,18 @@ global.APIKeys = {
 }
 
 // Other
-global.owner = ['6281575886399','6281575886399','6281575886399']
-global.premium = ['6281575886399']
-global.ownernomer = '6281575886399'
-global.ownername = 'RiychDwayne'
-global.botname = 'Chika-Md'
-global.footer = 'RiychDwayne'
-global.ig = 'https://instagram.com/riychdwayne'
+global.owner = ['6281284843853','6281284843853','62812848483853']
+global.premium = ['6281284843853']
+global.ownernomer = '6281284843853'
+global.ownername = 'Fwz-' 
+global.botname = 'Fwz-'
+global.footer = 'Fwz'
+global.ig = 'https://instagram.com/fwztzy'
 global.email = 'riychh.20@gmail.com'
 global.region = 'Indonesia'
 global.sc = 'https://youtube.com/channel/UCtM-pDaaGVRe4BJ7w4qE4Bw'
 global.myweb = 'https://api-riychdwayne.herokuap.com'
-global.packname = 'Chika-Md'
+global.packname = 'Fwz-MD'
 global.author = 'WhatsApp Bot'
 global.sessionName = 'chika'
 global.prefa = ['','!','.','🐦','🐤','🗿']
